e3aa328af0f631f62a06215dc4ac374136ca2a8f
Girijesh Prasad <142652247+girijesh-ai@users.noreply.github.com>
2023-08-19 21:27:31 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -42,7 +42,7 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest'][0])
+            st.write(podcast_info['podcast_guest'])
 
         with col4:
             st.subheader("Podcast Guest Details")
